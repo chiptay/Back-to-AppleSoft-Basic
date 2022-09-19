db0dad2fd9f3ed2ad3cb4f41c2a0131ebf1b45ef
chiptay <113942109+chiptay@users.noreply.github.com>
2022-09-19 15:09:15 -0500

    Add files via upload

---
+++ hello_broadcast_system.py
@@ -0,0 +1,31 @@
+# ChipTay Python Sandbox 9.19.2022
+# Copyright 2022 Chip Taylor
+# 
+
+# print("Hello World!")
+# \t tab \n newline
+#declare variables
+line_one = "\tThis is a test"
+line_two = 'of the emergency broadcast system'
+line_three = "this is only a test"
+line_four = line_one
+#favorite_language = 'python'
+
+
+# print out stuff
+print(line_one.upper())
+print(line_two.upper())
+print(line_three.upper())
+print(line_four.strip()
+         .capitalize()
+         .upper())
+print("\n")
+print(f"{line_one.upper()} {line_two.capitalize()}. \n{line_three.upper()}!")
+
+print(f"{line_one} {line_two.capitalize()}. \n{line_three.upper()}!")
+
+print(f"{line_one} {line_two.lower()}. \n{line_three.upper()}!")
+
+print(f"{line_one.lower()}{line_two.capitalize()}. \n{line_three.upper()}!")
+  
+   
